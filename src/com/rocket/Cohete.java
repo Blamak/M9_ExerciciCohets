25a26c95e53704d47945c69d864912a752c610dd
Blamak <vlasherassanchez@gmail.com>
2021-02-03 13:18:11 +0100

    Final commit Fase1

--- src/com/rocket/Cohete.java
+++ src/com/rocket/Cohete.java
@@ -27,18 +27,15 @@ public class Cohete {
 		Cohete rocket2;
 		try {
 			rocket1 = new Cohete("LDSFJA32", 6);
-			System.out.println("Output: \n" + rocket1.getCode() + ": " + rocket1.getNumPropulsores() + " propulsores");
+			System.out
+					.println("Output: \n" + rocket1.getCode() + ": " + rocket1.getNumPropulsores() + " propulsores");
 
 			rocket2 = new Cohete("x", 3);
-			System.out.println("Output: \n" + rocket2.getCode() + ": " + rocket2.getNumPropulsores() + " propulsores\n");
-			
+			System.out
+					.println("Output: \n" + rocket2.getCode() + ": " + rocket2.getNumPropulsores() + " propulsores\n");
+
 		} catch (Exception e) {
 			System.err.println("Error, código debe tener 8 dígitos.");
 		}
-		
-		
-
-
 	}
-
 }
